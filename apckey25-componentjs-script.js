b7e9880abf19bff7ef67b85763d66acc5c60cc88
Thiago Colen <thiago.souzacolen@gmail.com>
2023-02-20 17:41:20 -0300

    feat: Change Deck Funcionality
    to PLAY and all KNOBs of APCKey25
    
    controling Mixxx EQ, VOLUME, FILTER and EFFECTS

--- apckey25-componentjs-script.js
+++ apckey25-componentjs-script.js
@@ -8,6 +8,8 @@ var ApcKey25 = {};
 ApcKey25.init = function (id, debugging) {
     print("----->>> init()");
     print("----->>> id:" + id + " debugging:" + debugging);
+
+    ApcKey25.initSoftTakeover();
 };
 
 ApcKey25.shutdown = function () {
@@ -16,6 +18,7 @@ ApcKey25.shutdown = function () {
 
 // -------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------
+// UTILS $ CONSTANTS
 // -------------------------------------------------------------------------------
 
 // TODO: remove this shit or make it better
@@ -27,7 +30,37 @@ ApcKey25.printParameters = function (channel, control, value, status, group) {
     print("------>>> group:" + group);
 };
 
-Led_Color = {
+ApcKey25.initSoftTakeover = function () {
+    for (i = 48; i <= 55; i++) {
+        engine.softTakeover(
+            KNOBS_TO_CONTROL[i].group(1),
+            KNOBS_TO_CONTROL[i].control,
+            true
+        );
+        engine.softTakeover(
+            KNOBS_TO_CONTROL[i].group(2),
+            KNOBS_TO_CONTROL[i].control,
+            true
+        );
+    }
+};
+
+ApcKey25.initSoftTakeoverIgnore = function () {
+    for (i = 48; i <= 55; i++) {
+        engine.softTakeoverIgnoreNextValue(
+            KNOBS_TO_CONTROL[i].group(1),
+            KNOBS_TO_CONTROL[i].control
+        );
+        engine.softTakeoverIgnoreNextValue(
+            KNOBS_TO_CONTROL[i].group(2),
+            KNOBS_TO_CONTROL[i].control
+        );
+    }
+};
+
+// -------------------------------------------------------------------------------
+
+LED_COLOR = {
     off: 0x00,
     green: 0x01,
     blinkGreen: 0x02,
@@ -37,7 +70,9 @@ Led_Color = {
     blinkYellow: 0x06,
 };
 
-Pad_Matrix = {
+// TODO: complete the keys of PAD_MATRIX
+
+PAD_MATRIX = {
     scene_1: {
         1: 0x20,
         2: 0x21,
@@ -51,12 +86,12 @@ Pad_Matrix = {
     scene_2: {
         1: 0x18,
         2: 0x19,
-        3: 0x1A,
-        4: 0x1B,
-        5: 0x1C,
-        6: 0x1D,
-        7: 0x1E,
-        8: 0x1F,
+        3: 0x1a,
+        4: 0x1b,
+        5: 0x1c,
+        6: 0x1d,
+        7: 0x1e,
+        8: 0x1f,
     },
     scene_3: {
         1: 0x10,
@@ -71,14 +106,14 @@ Pad_Matrix = {
     scene_4: {
         1: 0x08,
         2: 0x09,
-        3: 0x0A,
-        4: 0x0B,
-        5: 0x0C,
-        6: 0x0D,
-        7: 0x0E,
-        8: 0x0F,
+        3: 0x0a,
+        4: 0x0b,
+        5: 0x0c,
+        6: 0x0d,
+        7: 0x0e,
+        8: 0x0f,
     },
-	scene_5: {
+    scene_5: {
         1: 0x00,
         2: 0x01,
         3: 0x02,
@@ -88,7 +123,66 @@ Pad_Matrix = {
         7: 0x06,
         8: 0x07,
     },
+};
 
+// TODO: Which keys can we map to a JS Object?
+
+MIDI_VALUES = {
+    note_off: 0x80,
+    note_on: 0x90,
+    control: 0xb0,
+    pitch: 0xe0,
+};
+
+var KNOBS_TO_CONTROL = {
+    48: {
+        group: function (n) {
+            return "[Channel" + n + "]";
+        },
+        control: "volume",
+    },
+    49: {
+        group: function (n) {
+            return "[Channel" + n + "]";
+        },
+        control: "filterHigh",
+    },
+    50: {
+        group: function (n) {
+            return "[Channel" + n + "]";
+        },
+        control: "filterMid",
+    },
+    51: {
+        group: function (n) {
+            return "[Channel" + n + "]";
+        },
+        control: "filterLow",
+    },
+    52: {
+        group: function (n) {
+            return "[QuickEffectRack1_[Channel" + n + "]]";
+        },
+        control: "super1",
+    },
+    53: {
+        group: function (n) {
+            return "[EffectRack1_EffectUnit" + n + "_Effect1]";
+        },
+        control: "meta",
+    },
+    54: {
+        group: function (n) {
+            return "[EffectRack1_EffectUnit" + n + "_Effect2]";
+        },
+        control: "meta",
+    },
+    55: {
+        group: function (n) {
+            return "[EffectRack1_EffectUnit" + n + "_Effect3]";
+        },
+        control: "meta",
+    },
 };
 
 // -------------------------------------------------------------------------------
@@ -98,8 +192,6 @@ Pad_Matrix = {
 ApcKey25.isShiftPress = false;
 
 ApcKey25.Shift = function (channel, control, value, status, group) {
-    ApcKey25.printParameters(channel, control, value, status, group);
-
     if (status === 144) {
         ApcKey25.isShiftPress = true;
     } else {
@@ -112,17 +204,17 @@ ApcKey25.Shift = function (channel, control, value, status, group) {
 // -------------------------------------------------------------------------------
 
 ApcKey25.Play = function (channel, control, value, status, group) {
-    ApcKey25.printParameters(channel, control, value, status, group);
     script.toggleControl(group, "play");
 };
 
 var syncPlayOutputCallback = function (value, group, control) {
     var parameter = engine.getParameter("[Channel1]", "play");
-    if (parameter === 1) {
-        midi.sendShortMsg(0x90, Pad_Matrix.scene_5[2], Led_Color.blinkGreen);
-    } else {
-        midi.sendShortMsg(0x90, Pad_Matrix.scene_5[2], Led_Color.off);
-    }
+
+    midi.sendShortMsg(
+        MIDI_VALUES.note_on,
+        PAD_MATRIX.scene_5[2],
+        parameter === 1 ? LED_COLOR.blinkGreen : LED_COLOR.off
+    );
 };
 
 engine.makeConnection("[Channel1]", "play", syncPlayOutputCallback);
@@ -131,15 +223,82 @@ engine.makeConnection("[Channel1]", "play", syncPlayOutputCallback);
 // -------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------
 
-// engine.getParameter and engine.setParameter work
-// with values on a scale from 0 to 1.
-// -------------------------------------------------------
-// engine.getParameter(string group, string key);
-// engine.setParameter(string group, string key, double newValue);
-//
-//
-// The engine.get/setValue functions should be used for Controls
-// with discrete states like orientation.
-// -------------------------------------------------------
-// engine.getValue(string group, string key);
-// engine.setValue(string group, string key, double newValue);
+ApcKey25.activeDeckObj = {
+    name: "Deck1",
+    control: "enabled",
+    group: "[Master]",
+};
+
+ApcKey25.changeActiveDeck = function (channel, control, value, status, group) {
+    var parameter = engine.getParameter(
+        ApcKey25.activeDeckObj.group,
+        ApcKey25.activeDeckObj.control
+    );
+
+    ApcKey25.activeDeckObj.name = parameter === 1 ? "Deck1" : "Deck2";
+
+    engine.setParameter(
+        ApcKey25.activeDeckObj.group,
+        ApcKey25.activeDeckObj.control,
+        parameter === 1 ? 0 : 1
+    );
+
+    ApcKey25.initSoftTakeoverIgnore();
+};
+
+var syncActiveDeckOutputCallback = function (value, group, control) {
+    var parameter = engine.getParameter(
+        ApcKey25.activeDeckObj.group,
+        ApcKey25.activeDeckObj.control
+    );
+
+    midi.sendShortMsg(
+        MIDI_VALUES.note_on,
+        PAD_MATRIX.scene_5[3],
+        parameter === 1 ? LED_COLOR.off : LED_COLOR.red
+    );
+
+    midi.sendShortMsg(
+        MIDI_VALUES.note_on,
+        PAD_MATRIX.scene_5[7],
+        parameter === 1 ? LED_COLOR.red : LED_COLOR.off
+    );
+};
+
+engine.makeConnection(
+    ApcKey25.activeDeckObj.group,
+    ApcKey25.activeDeckObj.control,
+    syncActiveDeckOutputCallback
+);
+
+// -------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------
+
+ApcKey25.knob = function (channel, control, value, status, group) {
+    ApcKey25.printParameters(channel, control, value, status, group);
+
+    engine.setValue(
+        KNOBS_TO_CONTROL[control].group(
+            ApcKey25.activeDeckObj.name === "Deck1" ? "1" : "2"
+        ),
+        KNOBS_TO_CONTROL[control].control,
+        script.absoluteLin(value, 0, 2)
+    );
+};
+
+ApcKey25.filterKnob = function (channel, control, value, status, group) {
+    ApcKey25.printParameters(channel, control, value, status, group);
+
+    engine.setValue(
+        KNOBS_TO_CONTROL[control].group(
+            ApcKey25.activeDeckObj.name === "Deck1" ? "1" : "2"
+        ),
+        KNOBS_TO_CONTROL[control].control,
+        script.absoluteLin(value, 0, 1)
+    );
+};
+
+// -------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------
